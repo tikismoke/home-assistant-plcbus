e8062289e3ed07033ab74df4d468d374dbe29bfc
tikismoke <webmaster@poudot.fr>
2020-05-09 20:48:02 +0200

    Update switch.py

--- switch.py
+++ switch.py
@@ -2,20 +2,13 @@
 import logging
 from typing import Optional
 
-
+import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
-
+from homeassistant.components.switch import PLATFORM_SCHEMA
+from homeassistant.helpers.entity import ToggleEntity
 from lib.plcbus_lib import PLCBUSAPI, PLCBUSException
 
-from homeassistant.components.switch import DOMAIN
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.helpers.typing import HomeAssistantType
-import homeassistant.helpers.config_validation as cv
-from homeassistant.const import STATE_OFF, STATE_ON
-
 _LOGGER = logging.getLogger(__name__)
-
-ENTITY_ID_FORMAT = DOMAIN + ".{}"
 
 DOMAIN = "plcbus"
 
@@ -24,8 +17,8 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     Api = PLCBUSAPI(logging,"/dev/ttyUSB0",commandCB,messageCB)
     entities = []
     house="D1"
-    device="A3"
-    for devices in device:
+    devices="A3"
+    for device in devices:
         entities.append(PlcbusSwitch(Api, device, house))
     add_entities(entities, True)
     return True
@@ -34,13 +27,13 @@ def commandCB(self):
     print("commandCB")
     if self['d_command']=="GET_ALL_ID_PULSE":
         print ("get all id pulse reponse",self)
-        for entity in entities:
+        for entity in PlcbusSwitchList:
             print (entity.name)
 
     else:
         print (self)
         print("Current status for %s, is %s", self['d_home_unit'], self['d_command'])
-        for entity in entities:
+        for entity in PlcbusSwitchList:
             if (entity._device_code == self['d_home_unit']) :
                 print("Device exists:")
                 print (entity.name)
@@ -52,16 +45,11 @@ def commandCB(self):
                     entity.set_state(True)
                 elif (self['d_command'] == "OFF") :
                     entity.set_state(False)
-            else :
-                print("status for other device")
-                print (entity.name)
-
 
 def messageCB(self):
     print ("messageCB")
-    print ("messageCB")
 
-class PlcbusSwitch():
+class PlcbusSwitch(ToggleEntity):
     """Representation of a Plcbus switch."""
 
     def __init__(self,plcbus_API, device_code, house_code) -> None:
@@ -98,4 +86,3 @@ class PlcbusSwitch():
     def update(self):
         """Get the state and update it."""
         self._plcbus_API.send("STATUS_REQUEST",self._device_code,self._house_code)
-
